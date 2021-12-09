bdb1e1a88705828c0091231423598dd14458b9aa
Stanley Gao <stanley.s.gao.24@dartmouth.edu>
2021-12-09 12:59:01 -0500

    working day 9 part 1

--- day9/day9p2.py
+++ day9/day9p2.py
@@ -10,7 +10,7 @@ for string in input:
   vals.append(string)
 # access with vals[y][x]
 
-sum = 0
+low_points = []
 
 for y in range(len(vals)):
   for x in range(len(vals[0])):
@@ -26,6 +26,41 @@ for y in range(len(vals)):
     # right check
     if x < len(vals[0]) - 1 and vals[y][x] >= vals[y][x+1]:
       continue
-    sum += vals[y][x] + 1
+    
+    low_points.append((x, y))
 
-print(sum)
+def good_neighbor(p, p_seen) -> list[tuple]: 
+  x, y = p[0], p[1]
+  proper_flow = True
+  # upper check
+  if y > 0:
+    n_up = vals[y-1][x]
+  else:
+    n_up = 10
+  # lower check
+  if y < len(vals) - 1:
+    n_down = vals[y+1][x]
+  else:
+    n_down = 10
+  # left check
+  if x > 0:
+    n_left = vals[y][x-1]
+  else:
+    n_left = 10
+  # right check
+  if x < len(vals[0]) - 1:
+    n_right = vals[y][x+1]
+  else:
+    n_right = 10
+  
+
+def find_basin(point):
+  global vals
+  points_to_check = []
+  points_seen = []
+  points_to_check.append(point)
+  while good_neighbor(point):
+    pass
+  
+
+print(low_points)
